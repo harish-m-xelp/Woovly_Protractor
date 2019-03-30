e8d5703eec3e52e4c1385a2ef7ca339e5e2a4436
Harish <harish@xelpmoc.in>
2019-03-30 13:38:02 +0530

    created test branch

--- Specs/Login.js
+++ Specs/Login.js
@@ -8,13 +8,15 @@ describe('Woovly Login Module',  function() {
       await login.Get_Email_Login(data.config.params.url,data.config.params.userEmailid,data.config.params.userEmailPass);
       browser.sleep(3000);
       expect(await browser.getTitle()).toEqual('test.14 - Feed');
+      browser.sleep(3000);
       console.log("Logged in Successfully...");
+      await login.logOut();
     });
 
-    it('Verify Title After Logout',async function() {
-      await login.logOut();
-      browser.sleep(3000);
-      expect(await browser.getTitle()).toEqual('Woovly.com - Login or Sign Up - To Create Your Bucket List');
-    });
+    // it('Verify Title After Logout',async function() {
+    //   await login.logOut();
+    //   browser.sleep(3000);
+    //   expect(await browser.getTitle()).toEqual('Woovly.com - Login or Sign Up - To Create Your Bucket List');
+    // });
 
 });
